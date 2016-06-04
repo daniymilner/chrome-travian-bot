3ceabe97e8ac1c979d329ee7e0deff486a3e8da8
Aleksey Olenyuk <alo@qapint.com>
2016-06-04 20:58:09 +0300

    change file structure, add dorf page script

---
+++ index.js
@@ -0,0 +1,6 @@
+(function(){
+	var travianBot = document.createElement('div');
+	travianBot.id = 'travianBot';
+	travianBot.innerHTML = '<div id="travianBotPanel"></div>';
+	document.body.appendChild(travianBot);
+})();
---
+++ js/pages/dorf1.js
@@ -0,0 +1,27 @@
+(function(){
+	var travianBotPanel = document.getElementById('travianBotPanel'),
+		buldings = document.createElement('div');
+	buldings.id = 'travianBotBuildings';
+	buldings.innerHTML =
+		'<ul>' +
+		'<li id="build_1"><p>Лісоповал</p></li>' +
+		'<li id="build_2"><p>Ферма</p></li>' +
+		'<li id="build_3"><p>Лісоповал</p></li>' +
+		'<li id="build_4"><p>Залізна копальня</p></li>' +
+		'<li id="build_5"><p>Глиняний кар`єр</p></li>' +
+		'<li id="build_6"><p>Глиняний кар`єр</p></li>' +
+		'<li id="build_7"><p>Залізна копальня</p></li>' +
+		'<li id="build_8"><p>Ферма</p></li>' +
+		'<li id="build_9"><p>Ферма</p></li>' +
+		'<li id="build_10"><p>Залізна копальня</p></li>' +
+		'<li id="build_11"><p>Залізна копальня</p></li>' +
+		'<li id="build_12"><p>Ферма</p></li>' +
+		'<li id="build_13"><p>Ферма</p></li>' +
+		'<li id="build_14"><p>Лісоповал</p></li>' +
+		'<li id="build_15"><p>Ферма</p></li>' +
+		'<li id="build_16"><p>Глиняний кар`єр</p></li>' +
+		'<li id="build_17"><p>Лісоповал</p></li>' +
+		'<li id="build_18"><p>Глиняний кар`єр</p></li>' +
+		'</ul>';
+	travianBotPanel.appendChild(buldings);
+})();
