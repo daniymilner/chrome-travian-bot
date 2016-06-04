796fa14c394423bad65579de1657c83a2dec0e08
Oleksandr Olenyuk <sanya.olenyuk@gmail.com>
2016-06-04 21:25:17 +0300

    upload index

--- index.js
+++ index.js
@@ -3,4 +3,8 @@
 	travianBot.id = 'travianBot';
 	travianBot.innerHTML = '<div id="travianBotPanel"></div>';
 	document.body.appendChild(travianBot);
+	var style = document.createElement('style');
+	document.head.appendChild(style);
+	style.type = 'text/css';
+	style.innerText = '#travianBot{position:absolute;top:0;left:0;background:#0033FF;color:#fff;z-index:1000000;}';
 })();
