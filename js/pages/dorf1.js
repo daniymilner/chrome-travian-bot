02a7b041e54165a944ba1023353d6bdbff7f09e1
Aleksey Olenyuk <alo@qapint.com>
2016-06-04 21:41:32 +0300

    refactor creating dorf1 page

--- js/pages/dorf1.js
+++ js/pages/dorf1.js
@@ -1,27 +1,14 @@
 (function(){
 	var travianBotPanel = document.getElementById('travianBotPanel'),
-		buldings = document.createElement('div');
-	buldings.id = 'travianBotBuildings';
-	buldings.innerHTML =
-		'<ul>' +
-			'<li id="build_1"><p></p></li>' +
-			'<li id="build_2"><p></p></li>' +
-			'<li id="build_3"><p></p></li>' +
-			'<li id="build_4"><p></p></li>' +
-			'<li id="build_5"><p></p></li>' +
-			'<li id="build_6"><p></p></li>' +
-			'<li id="build_7"><p></p></li>' +
-			'<li id="build_8"><p></p></li>' +
-			'<li id="build_9"><p></p></li>' +
-			'<li id="build_10"><p></p></li>' +
-			'<li id="build_11"><p></p></li>' +
-			'<li id="build_12"><p></p></li>' +
-			'<li id="build_13"><p></p></li>' +
-			'<li id="build_14"><p></p></li>' +
-			'<li id="build_15"><p></p></li>' +
-			'<li id="build_16"><p></p></li>' +
-			'<li id="build_17"><p></p></li>' +
-			'<li id="build_18"><p></p></li>' +
-		'</ul>';
-	travianBotPanel.appendChild(buldings);
+		buildings = document.createElement('div'),
+		buildingsLength = 18,
+		buildingsHtml;
+	buildings.id = 'travianBotBuildings';
+	buildingsHtml = '<ul>';
+	for(var i = 1; i <= buildingsLength; i++){
+		buildingsHtml += '<li id="build_' + i + '"><p></p></li>'
+	}
+	buildingsHtml += '</ul>';
+	buildings.innerHTML = buildingsHtml;
+	travianBotPanel.appendChild(buildings);
 })();
